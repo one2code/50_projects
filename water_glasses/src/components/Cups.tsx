26af6e123c3c83d403b8d6124f2b539a0a2e7ff9
Travis Bennett <t_bennett_a@protonmail.com>
2023-01-23 23:33:24 -0500

    Modified scss + added logic 4 small cup selection

--- water_glasses/src/components/Cups.tsx
+++ water_glasses/src/components/Cups.tsx
@@ -1,8 +1,22 @@
-function Cups() {
+import {useState} from 'react'
+
+
+
+const Cups: React.FC = () => {
+
+    const [selected, setSelected] = useState<number[]>([])
+
+    const handleClick = (index:number) => {
+        if (selected.includes(index)) {
+            setSelected(selected.filter((item) => item !== index))
+        } else {
+            setSelected([...selected, index])
+        }
+    }
 	return (
 		<>
 			<div className="cup">
-				<div className="remained" id="remained">
+				<div className="remaining" id="remaining">
 					<div className="span" id="litres">
 						1.5L
 					</div>
@@ -16,14 +30,11 @@ function Cups() {
 				Select how many glasses of water you've had
 			</p>
             <div className="cups">
-                <div className="cup cup-small full">250 ml</div>
-                <div className="cup cup-small full">250 ml</div>
-                <div className="cup cup-small">250 ml</div>
-                <div className="cup cup-small">250 ml</div>
-                <div className="cup cup-small">250 ml</div>
-                <div className="cup cup-small">250 ml</div>
-                <div className="cup cup-small">250 ml</div>
-                <div className="cup cup-small">250 ml</div>
+                {Array(8).fill(null).map((_, index) => (
+                    <div className={`cup cup-small ${selected.includes(index) ? 'full' : ''}`} onClick={() => handleClick(index)} key={index}>
+                        250 ml
+                    </div>
+                ))}
             </div>
 		</>
 	);
